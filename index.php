46fbd00487309f69cd8d1da9871f745a3c63718f
pmecpmec <70710293+pmecpmec@users.noreply.github.com>
2023-09-11 01:50:25 +0200

    Opdracht 1 tot en met 3

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="style.css">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Assignments College 01 Periode 01</title>
+</head>
+
+<body>
+  <?php 
+    $person = array(   "firstName" => "Pedro",
+      "secondName" => "Miguel",
+      "lastName" => "Eduardo Cardoso",);
+
+    $hobbies = array("sports" => "Fitness",
+      "gaming" => "Counter-Strike: Global Offensive",);
+
+    $pets = array("pets" => "2 dogs",
+      "petsName" => "Daisy and Snoopy",);
+
+    $favorites = array("favoriteFood" => "Wraps",
+      "favoriteMusicGenre" => "Rap",
+      "favoriteSeries" => "One Piece",
+      "favoriteVideoGame" => "Counter-Strike: Global Offensive");
+
+
+    
+    echo "<h1>My name is $person[firstName] </h1>";
+
+
+  ?>
+</body>
+
+
+</html>
