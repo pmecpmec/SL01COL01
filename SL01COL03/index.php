0a7c1377be2c8c0b306dd1b27eeb4f923a9cf9d9
pmecpmec <70710293+pmecpmec@users.noreply.github.com>
2023-09-20 10:21:44 +0200

    commit COL03

---
+++ SL01COL03/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+ * Schrijf een functie waarbij de kleur van de pagina
+ * bepaald wordt op basis van tijd
+*/
+
+/**
+ * Schrijf een functie waarbij de waarde van een variable getoond wordt, hoeveel het
+ * er zijn en welk type het is.
+ */
+
+/**
+ * Schrijf een functie waarbij een array random een andere waarde toont vanuit de array
+ */
+
+/**
+ * Verplaats alles naar een function directory, en require de function file.
+ */
+
+/**
+ * Haal de presentatie van een variabele binnen
+ */
